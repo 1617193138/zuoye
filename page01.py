640ccff058a868f9ffd4ad28bd4f2c7bdf41652a
nycht <552010694@qq.com>
2018-01-11 11:22:44 +0800

    zuoye

---
+++ page01.py
@@ -0,0 +1,34 @@
+#-*- coding:utf-8 –*-
+import urllib
+import urllib2
+import re
+import thread
+import time
+
+class page1:
+    def __init__(self):
+        self.user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+        self.url = 'http://www.zjjrc.com/job/list/0-0-0-0_0_0_0_0_0-0-0-0-1.html'
+        self.headers = {'User-Agent':self.user_agent}
+
+    def getpage(self, index):
+        user_agent=self.user_agent
+        url=self.url
+        headers=self.headers
+        result = urllib2.Request( url, headers=headers )
+        response = urllib2.urlopen(result)
+        pageCode = response.read()
+        pattern = re.compile(r'<a href=.*?class="search_job_jobs_name yun_text_color".*?>(.*?)</a>.*?<span class="job_search_xz">(.*?)</span>.*?<em class="com_search_job_em">(.*?)</em>.*?<em class="com_search_job_em">(.*?)</em>.*?<em class="com_search_job_em">(.*?)</em>',re.S)
+        items = re.findall(pattern, pageCode)
+        for i in items:
+            input = raw_input()
+            for g in i:
+                print g
+
+
+
+    def start(self):
+        print "--------------------------------------page1------------------------------------"
+        print "-------------             i'm    Separation   line         --------------------"
+        self.getpage(index=page1)
+        
---
+++ start.py
@@ -0,0 +1,12 @@
+from page01 import page1
+from page02 import page2
+from page03 import page3
+from page04 import page4
+a=page1()
+a.start()
+b=page2()
+b.start()
+c=page3()
+c.start()
+d=page4()
+d.start()
